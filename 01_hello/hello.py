28d09372b27298e8c2fd86b1087b835297dca374
LawfulEv1l <fmartinezjr70@gmail.com>
2023-02-04 14:20:04 -0500

    test git

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-#say hello
+"""
+Author:  Federico Martinez
+Purpose: Say hello
+"""
 #print('hello World!')
-
 import argparse
 
-parser = argparse.ArgumentParser(description= 'Say Hello')
-parser.add_argument('name', help='name to greet')
+parser = argparse.ArgumentParser(description='Say Hello')
+parser.add_argument('-n',
+                    '--name',
+                    metavar="name",
+                    default='World',
+                    help='name to greet')
 args = parser.parse_args()
 name = args.name
 
-print( "Hello,"+ name + '!' )
+print("Hello, " + name + '!')
---
+++ 01_hello/testname.py
@@ -0,0 +1,8 @@
+#!/ur/bin/env/python3
+def main():
+    print('in maain of testname')
+    print('__name__=' + __name__)
+
+
+if __name__ == '__main__':
+    main()
